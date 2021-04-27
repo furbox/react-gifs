e85a57dfaaedb9cc4c325cd3530cac4b36cff22a
Christopher Flores <furbox@gmail.com>
2021-04-27 15:52:06 -0500

    up

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,14 @@
+export const getGifs = async (category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=ZTBjFGjQY4f794utt2z02v5vVoYg1E5o`;
+    const resp = await fetch(url);
+    const {data} = await resp.json();
+
+    const gifs = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    });
+    return gifs;
+}
